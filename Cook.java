31de89bc78c761e8cd73da636ed6b5018e29b4bf
Berke Yıldız <115038420+berkeydz@users.noreply.github.com>
2024-12-19 19:57:47 +0300

    added comments

--- Cook.java
+++ Cook.java
@@ -1,8 +1,11 @@
 public class Cook extends Employee{
     private double salary;
     private double taxRate;
+    //initialize tax rate here as 18.
 
     public Cook(int id, String name,double salary,double taxRate){
+        // The class diagram says that this function has parameters(int,String,double) so it should not take tax rate as a parameter it should have
+        //three parameters and the tax rate should be initialized at the beginning.
         super(id,name);
         this.salary =salary;
         this.taxRate = taxRate;
