f45dd8b8ae10d81ac7bf50e9a9ed61d0868362c5
basarcetin <basar.cetin@ozu.edu.tr>
2024-12-19 04:39:15 +0300

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,2 +1,22 @@
 import java.util.ArrayList;
-Public class Order
+Public class Order(){
+  Arraylist<Product>Orderlist=new Arraylist<>()
+  public void Addproduct(product){
+    Orderlist.add(product);
+  }
+  public void listOrder(){
+    for (int i=0;i<Orderlist.size();i++){
+      System.out.println(Orderlist.get(i);
+    }
+  }
+  public void getlist(list){
+    return list.get(i);
+  }
+  public void getOrderedProducts(){
+    return Orderlist;
+  }
+  public double calculatetotalPrice(){
+    for(int i=0;Orderlist.size();i++){
+      
+
+    
